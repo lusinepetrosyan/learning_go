44c5ab6ab33301fc831fbc6816cb9c6c5a9e04de
lusine <l.petrosyan@createch.am>
2018-11-06 16:31:41 +0400

    my first server

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func myHandlerFunc(w http.ResponseWriter, r *http.Request){
+	fmt.Fprint(w, "welcome to my site")
+}
 
 func main(){
-	fmt.Println("my project on go")
+	http.HandleFunc("/", myHandlerFunc)
+	http.ListenAndServe(":8000", nil)
 }
