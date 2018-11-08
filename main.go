ede9478c392c6160bdac39259d13f44fa81860f2
lusine <l.petrosyan@createch.am>
2018-11-08 17:03:49 +0400

    custom 404 error

--- main.go
+++ main.go
@@ -16,6 +16,11 @@ func contact(w http.ResponseWriter, r *http.Request){
 	fmt.Fprint(w, "<h1 color='brown'>this is my contact page</h1>")
 }
 
+func httpError(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1 color='red'>This is my first custom error page</h1>")
+}
+
 func fac(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h2>This is my FAQ page </h2>" +
@@ -27,6 +32,7 @@ func main(){
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/fac", fac)
+	r.NotFoundHandler = http.HandlerFunc(httpError)
 
 	http.ListenAndServe(":8000", r)
 }
