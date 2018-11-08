817b3e8da94040da466083cc0e15b9f086c35307
lusine <l.petrosyan@createch.am>
2018-11-08 16:47:15 +0400

    homework with gorila

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-func home(w http.ResponseWriter, r *http.Request){
+func home(w http.ResponseWriter, request *http.Request){
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1 color='green'>this is my home page</h1>")
 }
@@ -16,9 +16,17 @@ func contact(w http.ResponseWriter, r *http.Request){
 	fmt.Fprint(w, "<h1 color='brown'>this is my contact page</h1>")
 }
 
+func fac(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h2>This is my FAQ page </h2>" +
+		"<img src='/image.jpg' width='100' height='100'>")
+}
+
 func main(){
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/fac", fac)
+
 	http.ListenAndServe(":8000", r)
 }
