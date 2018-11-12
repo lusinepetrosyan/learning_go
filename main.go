cb4fadc2a455d28586a9169c2b9c8db8968b4cf0
lusine <l.petrosyan@createch.am>
2018-11-12 17:46:58 +0400

    println printf

--- main.go
+++ main.go
@@ -3,8 +3,31 @@ package main
 import "fmt"
 
 func main()  {
-	myArr := [6]int{1,2,3,14,5,6}
-	var t []int = myArr[1 : 4]  // 4 th index not included
+	var a string
+	var (  //other type of declaration variable
+		b int
+	 	c bool
+	)
+	d:= 1.4 //short declaration type of go
 
-	fmt.Println(t)
+	a, b, c = "apple", 12, true
+
+	fmt.Println("in printf %v can contains any existing type", a, b, c, d)
+	fmt.Println("%T type of variable i printf \n")
+	fmt.Println("%g float number but removes unnecessary fraction part \n")
+	fmt.Println("println no need to new line, it automatically sets")
+
+
+	fmt.Printf("my string is: %s \n my bool is: %t \n my int is: %d \n my float is: %f \n\n ", a, c, b, d)
+
+
+	fmt.Printf("float number %f \n", d)
+	fmt.Printf("float number without unnecessary fraction part %g \n\n", d)
+
+
+
+	fmt.Println("new line character is \\n")
+	fmt.Println("double cotes is \" haha done it \" ")
+
+
 }
