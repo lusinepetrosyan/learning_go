d4c7322e38234c95af3e845303b06bc31bbf28a9
lusine <l.petrosyan@createch.am>
2018-11-14 19:32:04 +0400

    FOR RANGE CLAUSE

--- main.go
+++ main.go
@@ -3,31 +3,21 @@ package main
 import "fmt"
 
 func main()  {
-	var a string
-	var (  //other type of declaration variable
-		b int
-	 	c bool
-	)
-	d:= 1.4 //short declaration type of go
+	 mySlice  := [4]string{"this", "is", "my", "slice"}
 
-	a, b, c = "apple", 12, true
+	for index, value := range mySlice {
+		fmt.Println("index:", index, "value:", value)
+	}
 
-	fmt.Println("in printf %v can contains any existing type", a, b, c, d)
-	fmt.Println("%T type of variable i printf \n")
-	fmt.Println("%g float number but removes unnecessary fraction part \n")
-	fmt.Println("println no need to new line, it automatically sets")
+	fmt.Println("\n++++++++++++++++++++++++++++\n")
 
+	for index := range mySlice{
+		fmt.Println("we can get only index", index)
+	}
 
-	fmt.Printf("my string is: %s \n my bool is: %t \n my int is: %d \n my float is: %f \n\n ", a, c, b, d)
+	fmt.Println("\n++++++++++++++++++++++++++++\n")
 
-
-	fmt.Printf("float number %f \n", d)
-	fmt.Printf("float number without unnecessary fraction part %g \n\n", d)
-
-
-
-	fmt.Println("new line character is \\n")
-	fmt.Println("double cotes is \" haha done it \" ")
-
-
+	for _, value := range mySlice{
+		fmt.Println("or only value, with using _", value)
+	}
 }
