29c0c87ad2e911a74d43498e85a849862053024e
lusine <l.petrosyan@createch.am>
2018-11-16 13:03:06 +0400

    anonymous function

--- main.go
+++ main.go
@@ -3,21 +3,7 @@ package main
 import "fmt"
 
 func main()  {
-	 mySlice  := [4]string{"this", "is", "my", "slice"}
-
-	for index, value := range mySlice {
-		fmt.Println("index:", index, "value:", value)
-	}
-
-	fmt.Println("\n++++++++++++++++++++++++++++\n")
-
-	for index := range mySlice{
-		fmt.Println("we can get only index", index)
-	}
-
-	fmt.Println("\n++++++++++++++++++++++++++++\n")
-
-	for _, value := range mySlice{
-		fmt.Println("or only value, with using _", value)
-	}
+	func(){
+		fmt.Printf("👍 say yas: this is anonymous function in Go")
+	}()
 }
